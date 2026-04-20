46a02ea61aa26ec0bedc681dc4760e1e5a8df958
Archie Warnock <warnock@awcubed.com>
2026-04-20 08:21:35 -0400

    Updated via pipx

--- IHWApp/IHWApp/settings.py
+++ IHWApp/IHWApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-h&ju)my_lvk27po@*6i)56*ojk^*_j)rccbqai7hp_joj9x('
+SECRET_KEY = 'django-insecure-2h%s$pefm%e7o)n(c&-%hpw%e&8x=64$f0@xq2po@vgdrc07^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
